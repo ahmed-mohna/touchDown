0afb149159f48e7b861df086161a688cbd681516
ahmed mohna <ahmedmohna995@gmail.com>
2022-06-03 23:48:36 +0300

    Initial Commit

---
+++ touchDown/touchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  touchDownApp.swift
+//  touchDown
+//
+//  Created by Ahmed Mohna on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct touchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
